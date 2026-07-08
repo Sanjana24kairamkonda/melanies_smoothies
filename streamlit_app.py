a6a290e0a137e7800499d2a328417530a0d46034
Sanjana24kairamkonda <96222161+Sanjana24kairamkonda@users.noreply.github.com>
2026-07-08 15:09:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,37 +19,24 @@ cnx = st.connection("snowflake")
 
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table('smoothies.public.fruit_options') \
+    .select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
-#st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+# Convert the Snowpark DataFrame to a Pandas DataFrame so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+
+st.dataframe(pd_df)
+
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
     my_dataframe,
     max_selections=5
 )
-
-if ingredients_list:
-    ingredients_string = ''
-
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-
-    #st.write(ingredients_string)
-
-    #my_insert_stmt = """ insert into smoothies.public.orders values ('""" + ingredients_string + """')"""
-
-    my_insert_stmt = """INSERT INTO smoothies.public.orders (ingredients, name_on_order)
-VALUES ('""" + ingredients_string + """', '""" + name_on_order + """')"""
-
-    #st.write(my_insert_stmt)
-
-    time_to_insert = st.button('Submit Order')
-
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-
-        st.success('Your Smoothie is ordered!', icon='✅')
 
 import requests
 
@@ -58,6 +45,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
 
